f8cbf9f9125b62a916daf40614267a2d468001e5
enteragoodusername <>
2026-02-16 18:52:28 -0500

    more changes

--- alarmServer/alarmServer.cpp
+++ alarmServer/alarmServer.cpp
@@ -1,24 +1,33 @@
 #include "alarmServer.hpp"
 #include "comm_declarations.hpp"
+#include "Arduino.h"
 
-
-AlarmServer::AlarmServer(uint8_t pin_cs_, uint8_t pin_rst_, uint8_t pin_irq_ ) : 
+AlarmServer::AlarmServer(uint8_t pin_cs_, uint8_t pin_rst_, int pin_irq_ = -1) : 
 	pin_cs(pin_cs_),
 	pin_rst(pin_rst_),
 	pin_irq(pin_irq_),
-	rf95(pin_cs_, pin_irq_)
+	rf95(pin_cs_, pin_irq)
 	{
 }
 bool AlarmServer::init(){
 	SPI.begin();
+	if (pin_rst != -1){
+		pinMode(pin_rst, OUTPUT);
+		digitalWrite(pin_rst, HIGH);
+		delay(10);
+		digitalWrite(pin_rst, LOW);
+		delay(10);
+		digitalWrite(pin_rst, HIGH);
+		delay(10);
+	}
 	Serial.println("Starting LoRa Module");
 	  if (!rf95.init()) {
-	Serial.println("RH_RF95 init failed. Check wiring/pins/power.");
+		Serial.println("RH_RF95 init failed. Check wiring/pins/power.");
 		return false;
 	}
 
 	if (!rf95.setFrequency(RF_FREQ_MHZ)) {
-	Serial.println("setFrequency failed (unsupported freq?)");
+		Serial.println("setFrequency failed (unsupported freq?)");
 		return false;
 	}
 
--- alarmServer/alarmServer.hpp
+++ alarmServer/alarmServer.hpp
@@ -2,12 +2,12 @@
 
 class AlarmServer {
 	public: 
-		AlarmServer(uint8_t pin_cs_, uint8_t pin_rst_, uint8_t pin_irq_);
+		AlarmServer(uint8_t pin_cs_, uint8_t pin_rst_, int pin_irq_);
 		bool init();
 	private:
-		RH_RF95 rf95;
 		uint8_t pin_cs;
-		uint8_t pin_rst;
+		int pin_rst;
 		uint8_t pin_irq;
+		RH_RF95 rf95;
 
 };
