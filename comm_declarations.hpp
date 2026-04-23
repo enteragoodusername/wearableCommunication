035025772c00bbb5e3e0537656aa176ab0f0e427
Matthew Brown <mattb7854@icloud.com>
2026-04-23 13:09:15 -0400

    double press -> manual alarm

--- comm_declarations.hpp
+++ comm_declarations.hpp
@@ -3,4 +3,7 @@ static constexpr uint8_t TX_POWER_DBM = 23;
 constexpr uint8_t MSG_HEARTBEAT = 1;
 constexpr uint8_t MSG_ALARM = 2;
 constexpr uint8_t MSG_ACK = 3;
-static const RH_RF95::ModemConfigChoice LORA_MODEM = RH_RF95::Bw125Cr45Sf128;
+// static const RH_RF95::ModemConfigChoice LORA_MODEM = RH_RF95::Bw125Cr45Sf128;
+static const RH_RF95::ModemConfigChoice LORA_MODEM = RH_RF95::Bw31_25Cr48Sf512;
+// static const RH_RF95::ModemConfigChoice LORA_MODEM = RH_RF95::Bw125Cr45Sf2048;
+
